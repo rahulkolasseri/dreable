dffb8f7d09f6535439d12083efb36122d89040a8
Rahul Kolasseri <rahul.kolasseri@gmail.com>
2022-09-16 16:23:24 +0800

    added photo upload via private channel

--- stablegram.py
+++ stablegram.py
@@ -1,6 +1,6 @@
 import os, logging, random, time
 from uuid import uuid4
-from telegram import Bot, Update, error, InlineQueryResultPhoto
+from telegram import Bot, Update, error, InlineQueryResultCachedPhoto
 from telegram.ext import CommandHandler, MessageHandler, filters, ContextTypes, ApplicationBuilder, InlineQueryHandler
 
 from dotenv import load_dotenv
@@ -12,6 +12,7 @@ logging.basicConfig(
     level=logging.DEBUG
 )
 
+
 async def replier(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.effective_chat != None:
         #messageID = update.message.message_id
@@ -19,24 +20,30 @@ async def replier(update: Update, context: ContextTypes.DEFAULT_TYPE):
         #messagecontent = update.message.text
         await context.bot.send_message(chat_id=update.effective_chat.id, text="hec")
 
+async def photoUpload(photoPath, prompt):
+    if prompt in photoLog.keys():
+        return photoLog[prompt]
+    else:
+        uploadedPhoto = await bot.send_photo(chat_id=os.getenv("CHATID"), photo=open(photoPath, 'rb'), caption=prompt)
+        photoLog[prompt] = uploadedPhoto
+        return uploadedPhoto
+
 async def inliner(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     query = update.inline_query.query
 
     if query == "":
         return
-    
+
+    photoList = [await photoUpload("A_small_elephant_playing_in_a_puddle_0.jpg", "test"), await photoUpload("A_small_elephant_playing_in_a_puddle_0.jpg", "test")]
+
     results = [
-                InlineQueryResultPhoto(
+                InlineQueryResultCachedPhoto(
                     id=uuid4(),
-                    title="URLPhoto",
-                    photo_url="https://upload.wikimedia.org/wikipedia/commons/d/d8/Macintosh_classic.jpg",
-                    thumb_url="https://upload.wikimedia.org/wikipedia/commons/d/d8/Macintosh_classic.jpg"
+                    photo_file_id= photoList[0]["photo"][-1]["file_id"]
                     ),
-                InlineQueryResultPhoto(
+                InlineQueryResultCachedPhoto(
                     id=uuid4(),
-                    title="URLPhoto",
-                    photo_url="https://upload.wikimedia.org/wikipedia/commons/d/d8/Macintosh_classic.jpg",
-                    thumb_url="https://upload.wikimedia.org/wikipedia/commons/d/d8/Macintosh_classic.jpg"
+                    photo_file_id= photoList[1]["photo"][-1]["file_id"]
                     )
                 ]
 
@@ -48,6 +55,9 @@ if __name__ == '__main__':
     start_time = time.time()
 
     application = ApplicationBuilder().token(os.getenv("TELEGRAM_TOKEN")).build()
+    bot = Bot(token=os.getenv("TELEGRAM_TOKEN"))
+
+    photoLog = {}
 
     replier_handler = MessageHandler(filters.TEXT, replier)
     inline_handler = InlineQueryHandler(inliner)
