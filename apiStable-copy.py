d39b4f8ed3916729ecacaaceb470cadaebced41a
rahulkolasseri <rahul.kolasseri@gmail.com>
2022-09-18 14:03:45 +0800

    apiStable updated to match Automatic1111's structure stablegram PhotoUpload fixed

--- apiStable-copy.py
+++ apiStable-copy.py
@@ -3,12 +3,7 @@ import threading
 
 from modules.paths import script_path
 
-import torch
-from omegaconf import OmegaConf
-
 import signal
-
-from ldm.util import instantiate_from_config
 
 from modules.shared import opts, cmd_opts, state
 import modules.shared as shared
@@ -24,76 +19,13 @@ import modules.extras
 import modules.lowvram
 import modules.txt2img
 import modules.img2img
-
-
-modules.codeformer_model.setup_codeformer()
-modules.gfpgan_model.setup_gfpgan()
-shared.face_restorers.append(modules.face_restoration.FaceRestoration())
-
-esrgan.load_models(cmd_opts.esrgan_models_path)
-realesrgan.setup_realesrgan()
-
-
-def load_model_from_config(config, ckpt, verbose=False):
-    print(f"Loading model [{shared.sd_model_hash}] from {ckpt}")
-    pl_sd = torch.load(ckpt, map_location="cpu")
-    if "global_step" in pl_sd:
-        print(f"Global Step: {pl_sd['global_step']}")
-    sd = pl_sd["state_dict"]
-
-    model = instantiate_from_config(config.model)
-    m, u = model.load_state_dict(sd, strict=False)
-    if len(m) > 0 and verbose:
-        print("missing keys:")
-        print(m)
-    if len(u) > 0 and verbose:
-        print("unexpected keys:")
-        print(u)
-    if cmd_opts.opt_channelslast:
-        model = model.to(memory_format=torch.channels_last)
-    model.eval()
-    return model
-
-# 
-# skipped stuff
-# 
-
+import modules.sd_models
 
 modules.scripts.load_scripts(os.path.join(script_path, "scripts"))
 
-try:
-    # this silences the annoying "Some weights of the model checkpoint were not used when initializing..." message at start.
+shared.sd_model = modules.sd_models.load_model()
 
-    from transformers import logging
-
-    logging.set_verbosity_error()
-except Exception:
-    pass
-
-with open(cmd_opts.ckpt, "rb") as file:
-    import hashlib
-    m = hashlib.sha256()
-
-    file.seek(0x100000)
-    m.update(file.read(0x10000))
-    shared.sd_model_hash = m.hexdigest()[0:8]
-
-sd_config = OmegaConf.load(cmd_opts.config)
-shared.sd_model = load_model_from_config(sd_config, cmd_opts.ckpt)
-shared.sd_model = (shared.sd_model if cmd_opts.no_half else shared.sd_model.half())
-
-if cmd_opts.lowvram or cmd_opts.medvram:
-    modules.lowvram.setup_for_low_vram(shared.sd_model, cmd_opts.medvram)
-else:
-    shared.sd_model = shared.sd_model.to(shared.device)
-
-modules.sd_hijack.model_hijack.hijack(shared.sd_model)
-
-# 
-#skipped stuff 
-#
-
-def text2image(prompt) -> None:
+def text2image(prompt, w=512, h=512) -> None:
     with threading.Lock():
-        images, js, info = modules.txt2img.txt2img(prompt, "", "", "", 20, 0, False, False, 1, 2, 7.0, -1, -1, 0.0, 0.0, 0.0, 512, 512, 0)
+        images, js, info = modules.txt2img.txt2img(prompt, "", "", "", 20, 0, False, False, 1, 2, 7.0, -1, -1, 0.0, 0.0, 0.0, w, h, 0)
     return images
--- stablegram.py
+++ stablegram.py
@@ -1,4 +1,4 @@
-import os, logging, random, time, sys
+import os, logging, random, time, sys, shlex
 from uuid import uuid4
 from io import BytesIO
 from telegram import Bot, Update, error, InlineQueryResultCachedPhoto
@@ -21,16 +21,22 @@ async def replier(update: Update, context: ContextTypes.DEFAULT_TYPE):
         #messagecontent = update.message.text
         await context.bot.send_message(chat_id=update.effective_chat.id, text="hec")
 
-async def photoUpload(photoMems, prompt):
+async def photoCreateUpload(prompt):
     if prompt in photoLog.keys():
         return photoLog[prompt]
     else:
         photoList = []
-        for i in range(2):
-            photoMem = photoMems[i]
-            uploadedPhoto = await bot.send_photo(chat_id=os.getenv("CHATID"), photo=photoMem, caption=prompt)
-            photoList.append(uploadedPhoto)
-        photoLog[prompt] = photoList
+        images = apiStable.text2image(prompt)
+        with BytesIO() as image1, BytesIO() as image2:
+            photoMems = [image1, image2]
+            for i in range(2):
+                images[i].save(photoMems[i], format="JPEG", quality=97)
+                photoMems[i].seek(0)
+            for i in range(2):
+                photoMem = photoMems[i]
+                uploadedPhoto = await bot.send_photo(chat_id=os.getenv("CHATID"), photo=photoMem, caption=prompt)
+                photoList.append(uploadedPhoto)
+            photoLog[prompt] = photoList
         return photoList
 
 async def inliner(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -40,14 +46,7 @@ async def inliner(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         return
     else:
         query = "hello jungle"
-        images = apiStable.text2image(query)
-        with BytesIO() as image1, BytesIO() as image2:
-            photoMems = [image1, image2]
-            for i in range(2):
-                images[i].save(photoMems[i], format="JPEG", quality=97)
-                photoMems[i].seek(0)
-            
-            photoList = await photoUpload([image1, image2], query)
+        photoList = await photoCreateUpload(query)
 
     results = [
                 InlineQueryResultCachedPhoto(
@@ -66,6 +65,7 @@ async def inliner(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 if __name__ == '__main__':
     print("Starting AUTOMATIC1111 launch checks...")
     sys.path += ['../../stablediff/stable-diffusion-webui/']
+    sys.argv += shlex.split("--opt-split-attention")
     import launch
     import apiStable
 
